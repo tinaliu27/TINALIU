1f188e5afe761045e570f93bf8323215ffb22b2b
tinaliu27 <27tinaliu@gmail.com>
2023-07-25 01:38:48 -0400

    added featured projects and footer section

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('scroll', () => {
-    const header = document.getElementById('header'); 
-    if(window.scrollY > 0) {
-        header.classList.add('scrolled'); 
-    } else {
-        header.classList.remove('scrolled');
-    }
-})
+const text = document.querySelector()
